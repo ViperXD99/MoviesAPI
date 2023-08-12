d8047cbe5256e350b2765500b39d13c5f755ebbd
ViperXD99 <sandaruxd99@gmail.com>
2023-08-12 14:16:16 +0530

    movies fetched from API

---
+++ MoviesApi/src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import SearchIcon from "./assets/search.svg";
+import MovieCard from "./MovieCard";
+const API_URL = "http://www.omdbapi.com?apikey=8fb2ed11";
+const movie1 = {
+  Title: "The Amazing Spiderman 2 Webb Cut",
+  Year: "2021",
+  imdbID: "tt18351128",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYzYzZDViNWYtNWViMS00NDMxLThlN2YtZjFkOWMwODkzNzhiXkEyXkFqcGdeQXVyMTUwMzM4NzU0._V1_SX300.jpg",
+};
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("spiderman");
+  }, []);
+  return (
+    <div className="app">
+      <h1>Movie Land</h1>
+      <div className="search">
+        <input
+          placeholder="Search for movies"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No Movies Found</h2>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
